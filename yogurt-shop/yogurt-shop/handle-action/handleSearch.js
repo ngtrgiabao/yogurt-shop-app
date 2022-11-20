822a604131ca9eb444d486784a637ee1a67ed171
Nguyen Tran Gia Bao <95952006+ngtrgiabaoB2012063@users.noreply.github.com>
2022-11-20 10:22:35 +0700

    Delete yogurt-shop/yogurt-shop directory

--- yogurt-shop/yogurt-shop/handle-action/handleSearch.js
+++
@@ -1,7 +0,0 @@
-const handleSearch = () => {
-    const url = new URL(window.location);
-    const ws = url.searchParams.get("words");
-    document.getElementById(
-        "searchDetails"
-    ).innerHTML = `<h1>Từ khóa cần tìm là: </h1> <p class="keywords"> ${ws} </p>`;
-};
--- yogurt-shop/yogurt-shop/validation/validationSearch.js
+++
@@ -1,6 +0,0 @@
-const validationSearch = () => {
-    const frm = document.forms["frm-search"];
-    if (frm.words.value.length > 0) {
-        frm.submit();
-    }
-};
